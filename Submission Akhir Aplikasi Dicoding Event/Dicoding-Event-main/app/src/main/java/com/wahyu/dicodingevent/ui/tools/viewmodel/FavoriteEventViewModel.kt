d40895f4612aff0be27fa55ea7ec488fd45d1d73
Multazam143 <multazam1005@gmail.com>
2025-01-10 04:40:59 +0800

    Memulai Pemrograman Dengan Kotlin
    Menambahkan Submission Akhir Memulai Pemrograman Dengan Kotlin

--- Submission Akhir Aplikasi Dicoding Event/Dicoding-Event-main/app/src/main/java/com/wahyu/dicodingevent/ui/tools/viewmodel/FavoriteEventViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.wahyu.dicodingevent.ui.tools.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.wahyu.dicodingevent.database.repository.FavoriteEventRepository
-
-class FavoriteEventViewModel(private val repository: FavoriteEventRepository) : ViewModel() {
-
-    fun getFavoriteEvent() = repository.getFavoriteEvent()
-}
--- Submission Akhir Aplikasi Dicoding Event/Dicoding-Event-main/app/src/main/java/com/wahyu/dicodingevent/utils/AppExecutors.kt
+++
@@ -1,8 +0,0 @@
-package com.wahyu.dicodingevent.utils
-
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-
-class AppExecutors {
-    val diskIO: Executor = Executors.newSingleThreadExecutor()
-}
